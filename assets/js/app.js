410a25b65e2a3c6209939d212b2e369d8528aa03
Michal <michal.raszka@outlook.com>
2018-11-27 20:18:42 +0100

    particles.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '../libs/particles.json', function() {
+particlesJS.load('particles-js', 'assets/libs/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
