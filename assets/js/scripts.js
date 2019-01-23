aa5072bf950b9e0bf48849ea4227d4b6bfbaf344
michal_raszka_ <michal.raszka@outlook.com>
2019-01-23 10:28:35 +0100

    #adjustments

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+
+/*Scroll to a href from nav*/
+    $("a[href^='#']").click(function(e) {
+        e.preventDefault();
+        
+        var position = $($(this).attr("href")).offset().top;
+    
+        $("body, html").animate({
+            scrollTop: position
+        });
+    });
+
+
